4f929cf70bd712b9054a5ea6f1fb0a48355180c9
David Gabriel Lacerda Dutra <davidlacerdadutra123@gmail.com>
2026-02-26 20:40:19 -0300

    .

--- scripts/criatura/recebe_data_criatura.gd
+++ scripts/criatura/recebe_data_criatura.gd
@@ -7,3 +7,11 @@ func _ready():
 	if CriaturaData and CriaturaData.modelo3D:
 		var instancia = CriaturaData.modelo3D.instantiate()
 		carregador_modelo3d.add_child(instancia)
+
+
+func _on_area_3d_mouse_entered() -> void:
+	print('mouse dentro')
+
+
+func _on_area_3d_mouse_exited() -> void:
+	print('mouse fora')
