6f787cf7352265ede46b39916d5ada34e8733605
David Gabriel Lacerda Dutra <davidlacerdadutra123@gmail.com>
2026-02-25 21:14:47 -0300

    first commit

---
+++ scripts/criatura/criatura_base.gd
@@ -0,0 +1,17 @@
+extends Resource
+
+class_name Criatura
+
+@export var patch_modelo3D : String
+@export var SpriteIcon : Texture
+
+##STATUS
+@export var nome: String
+@export var id:int
+@export var vida_atual:int
+@export var vida_maxima:int
+@export var precisao: String
+@export var velocidade: int
+@export var dano_base: int
+@export var exp_: int
+@export var octanara: int
---
+++ scripts/criatura/recebe_data_criatura.gd
@@ -0,0 +1,11 @@
+extends Node3D
+
+@export var teste: Criatura
+
+func _ready() -> void:
+	pass
+
+
+
+func _process(delta: float) -> void:
+	pass
